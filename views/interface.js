b38cb3f2ef9ce3fc780dc47404222dbb1efc0f06
Kane <kgin888@gmail.com>
2021-05-12 17:08:56 +0100

    created links to page

--- views/interface.js
+++ views/interface.js
@@ -6,10 +6,26 @@ document.addEventListener('DOMContentLoaded', () => {
     let content = document.querySelector('#new_note_text_box').value
     let noteId = noteContainer.create(content)
     let noteTitle = noteContainer.getTitleById(noteId)
-    var node = document.createElement('li');
-    var textnode = document.createTextNode(noteTitle);
-    node.appendChild(textnode);
-    document.getElementById('saved_notes_list').appendChild(node);
+    let listNode = document.createElement('li');
+    let linkNode = document.createElement('a')
+    linkNode.setAttribute('href', `#${noteId}`)
+    linkNode.innerText = noteTitle
+    listNode.appendChild(linkNode)
+    document.getElementById('saved_notes_list').appendChild(listNode);
     document.getElementById('new_note_text_box').value = null
+  })
+
+  window.addEventListener('hashchange', () => {
+    let url = window.location.hash.split('#')
+    if(url.length > 1) {
+      let noteId = url[1]
+      let noteContent = noteContainer.getContentById(parseInt(noteId))
+      document.querySelector('#main_page_display').style.display = 'none'
+      document.querySelector('#note_display').style.display = 'block'
+      document.querySelector('#note_display_content').innerText = noteContent
+    } else {
+      document.querySelector('#main_page_display').style.display = 'block'
+      document.querySelector('#note_display').style.display = 'none'
+    }
   })
 })
